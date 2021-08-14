7480c0afdbb91b924e060de9e032dc0ccb738e62
Mart <themcman@gmail.com>
2021-08-14 13:28:04 -0500

    saturday changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import json
 import random
 
 import discord
@@ -6,34 +7,70 @@ import discord
 TOKEN = os.environ.get("DISCORD_CHORROR_TOKEN")
 client = discord.Client()
 
+
+class Rarity:
+    EXTRA = 1
+    RARE = 10
+    UNCOMMON = 30
+    COMMON = 100
+
+    def from_str(s):
+        if s == "extra":
+            return Rarity.EXTRA
+        elif s == "rare":
+            return Rarity.RARE
+        elif s == "uncommon":
+            return Rarity.UNCOMMON
+        elif s == "common":
+            return Rarity.COMMON
+        else:
+            return None
+
+
+# get count
+chore_pool_count = 0
 with open("common", "r") as f:
-    common = [line for line in f.readlines()]
+    common = f.readlines()
 with open("uncommon", "r") as f:
-    uncommon = [line for line in f.readlines()]
+    uncommon = f.readlines()
 with open("rare", "r") as f:
-    rare = [line for line in f.readlines()]
-with open("extrarare", "r") as f:
-    extrarare = [line for line in f.readlines()]
+    rare = f.readlines()
+with open("extra", "r") as f:
+    extra = f.readlines()
+chore_pool_count += len(common) + len(uncommon) + len(rare) + len(extra)
+
+
+async def private_msg(message):
+    if message.content == "thanks":
+        await message.author.send("no problem")
+
+
+def has_decoration(msg):
+    return "||" in msg or "~~" in msg
 
 
 def format_msg(base_msg, rarity):
-    if "||" in base_msg or "~~" in base_msg:
-        # remove the newline character
+    if has_decoration(base_msg):
+        # REMOVE THE NEWLINE CHARACTER
         return base_msg[:-1]
 
-    if rarity == 1:
+    if rarity <= Rarity.EXTRA:
         # dark orange
         # msg = f"```css\n[ {base_msg[:-1]} ]```"
+
         # red
         msg = f"```diff\n- {base_msg[:-1]} -```"
-    elif rarity <= 10:
+
+    elif rarity <= Rarity.RARE:
         # blue
         msg = f"```md\n# {base_msg[:-1]} #```"
-    elif rarity <= 30:
+
+    elif rarity <= Rarity.UNCOMMON:
         # turquoise
         msg = f"```cs\n' {base_msg[:-1]} '```"
+
     else:
-        return base_msg
+        msg = f"```{base_msg[:-1]}```"
 
     return msg
 
@@ -45,32 +82,75 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    global chore_pool_count
 
-    if message.content.startswith("/hello"):
-        await message.channel.send("ahoy hoy")
+    if message.channel.type == discord.ChannelType.private:
+        await private_msg(message)
+
+    if message.content.lower().startswith("good morning, choretle"):
+        await message.channel.send("/me shrieks and beeps")
     elif message.content.startswith("/chore"):
         rarity = random.randint(1, 100)
 
-        if rarity == 1:
-            i = random.randint(0, len(extrarare) - 1)
-            msg = format_msg(extrarare[i], rarity)
+        if rarity <= Rarity.EXTRA:
+            with open("extra", "r") as f:
+                extra = [line for line in f.readlines()]
+            i = random.randint(0, len(extra) - 1)
+            msg = format_msg(extra[i], rarity)
 
-        elif rarity <= 10:
+        elif rarity <= Rarity.RARE:
+            with open("rare", "r") as f:
+                rare = [line for line in f.readlines()]
             i = random.randint(0, len(rare) - 1)
             msg = format_msg(rare[i], rarity)
 
-        elif rarity <= 30:
+        elif rarity <= Rarity.UNCOMMON:
+            with open("uncommon", "r") as f:
+                uncommon = [line for line in f.readlines()]
             i = random.randint(0, len(uncommon) - 1)
             msg = format_msg(uncommon[i], rarity)
 
         else:
+            with open("common", "r") as f:
+                common = [line for line in f.readlines()]
             i = random.randint(0, len(common) - 1)
-            msg = f"```{common[i]}```"
+            msg = format_msg(common[i], rarity)
 
         await message.channel.send(msg)
+        try:
+            with open(f"users/{message.author}.json", "r") as f:
+                data = json.load(f)
+            # data
+            with open(f"users/{message.author}.json", "w") as f:
+                f.write(json.dumps(data, indent=2))
+        except Exception as e:
+            print("error while doing json", e)
 
+    elif message.content.startswith("/add_c"):
+        await add_chore(message, "common")
 
+    elif message.content.startswith("/add_u"):
+        await add_chore(message, "uncommon")
+
+    elif message.content.startswith("/add_r"):
+        await add_chore(message, "rare")
+
+    elif message.content.startswith("/add_?"):
+        await add_chore(message, "extra")
+
+async def add_chore(message, rarity_s):
+    global chore_pool_count
+    words = message.content.split()[1:]
+    if len(words) == 0:
+        await message.author.send("...?")
+        await message.add_reaction("❌")
+        return
+    msg = " ".join(message.content.split()[1:]) + "\n"
+    display_msg = format_msg(f"{msg}", Rarity.from_str(rarity_s))
+    await message.author.send(f"adding \n{display_msg}")
+    with open(rarity_s, "a") as f:
+        f.write(msg)
+    await message.add_reaction("👍")
+    chore_pool_count += 1
 
 client.run(TOKEN)
