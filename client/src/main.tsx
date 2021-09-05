2fa9e58a7656eed505ddeaf25a5222ab91fbde0d
Mart <themcman@gmail.com>
2021-09-05 11:46:10 -0500

    table component

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+import Table from "./table";
+
 const Main = () => {
-  const [chores, setChores] = useState({
+  const [chores, setChores]: [
+    { common: []; uncommon: []; rare: []; spicy: [] },
+    Function
+  ] = useState({
     common: [],
     uncommon: [],
     rare: [],
     spicy: [],
-  });
-  const [visibiliy, setVisibility] = useState({
-    common: false,
-    uncommon: false,
-    rare: false,
-    spicy: false,
   });
 
   useEffect(() => {
@@ -43,59 +42,10 @@ const Main = () => {
           width: "600px",
         }}
       >
-        <div
-          className="commonTable"
-          style={{ display: "flex", flexDirection: "column" }}
-        >
-          <div
-            className="header"
-            style={{ display: "flex", flex: 1, flexDirection: "row" }}
-          >
-            <div style={{fontSize: "30px"}}>Common</div>
-            <div
-              style={{ display: "flex", cursor: 'pointer', flex: 1, fontSize: "50px", justifyContent: "flex-end", width: '50px' }}
-              onClick={(e) => {
-                console.log("click");
-                setVisibility({ ...visibiliy, common: !visibiliy.common });
-              }}
-            >
-              {visibiliy.common ? "v" : "Λ"}
-            </div>
-          </div>
-
-          {chores.common.map((text, i) => (
-            <div
-              style={{
-                border: "1px solid rgba(9,5,14,80)",
-                minHeight: "35px",
-                display: visibiliy.common ? "flex" : "none",
-                flexDirection: "row",
-              }}
-            >
-              <div
-                style={{
-                  borderRight: "1px solid rgba(69,69,69,69)",
-                  display: "flex",
-                  flex: 1,
-                  margin: "10px",
-                  maxWidth: "35px",
-                }}
-              >
-                {i}
-              </div>
-              <div
-                style={{
-                  display: "flex",
-                  flex: 1,
-                  justifyContent: "flex-start",
-                  margin: "10px",
-                }}
-              >
-                {text}
-              </div>
-            </div>
-          ))}
-        </div>
+        <Table chores={chores.common} rarity={"common"} />
+        <Table chores={chores.uncommon} rarity={"uncommon"} />
+        <Table chores={chores.rare} rarity={"rare"} />
+        <Table chores={chores.spicy} rarity={"spicy"} />
       </div>
     </div>
   );
