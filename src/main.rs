9bbac1102feb03d5e99801b120cbdda33bed64f5
Mart <themcman@gmail.com>
2022-03-31 00:06:22 -0500

    trying to init db

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,65 @@
 use mysql::prelude::*;
 use mysql::*;
 
+#[derive(Debug, PartialEq, Eq)]
+struct Chore {
+    id: i32,
+    text: String,
+    rarity: Rarity,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum Rarity {
+    Common,
+    Uncommon,
+    Rare,
+    Spicy,
+}
+
 fn main() {}
 
 #[test]
 fn it_works() {
-    #[derive(Debug, PartialEq, Eq)]
-    struct Payment {
-        customer_id: i32,
-        amount: i32,
-        account_name: Option<String>,
-    }
-
-    let opts = Opts::from_url(
-        "mysql://eldritch_chorror:af8287e3e2064f03bcd0584f0d01729a@wonk.gg:3306/eldritch_chorror",
-    )
-    .unwrap();
+    let opts = Opts::from_url("mysql://root:@localhost:3306").unwrap();
     let pool = Pool::new(opts).unwrap();
 
     let mut conn = pool.get_conn().unwrap();
 
-    conn.query_drop(
-        r"CREATE TABLE chores (
-        id INT NOT NULL,
-        text TEXT NOT NULL,
-        rarity ENUM('c','u','r','s'),
-        PRIMARY KEY id
-    );",
-    )
-    .unwrap();
+    conn.query_drop(r"CREATE DATABASE IF NOT EXISTS eldritch_chorror")
+        .unwrap();
+    conn.query_drop(r"USE eldritch_chorror").unwrap();
+
+    match conn.query_drop(
+        r"CREATE TABLE IF NOT EXISTS chores ( INT NOT NULL id,
+    //      PRIMARY KEY id)",
+        // )
+        // .unwrap();
+        // text TEXT NOT NULL,
+        // rarity ENUM('c','u','r','s'),
+        // ),
+    ) {
+        Ok(res) => {
+            println!("{:#?}", res);
+        }
+
+        _ => {
+            // println!("{:#?}", res);
+        }
+    };
+
+    let x = conn.query_iter(r"SELECT * FROM chores").unwrap();
+
+    println!("{:#?}", x);
+
+    // conn.query_drop(
+    //     r"CREATE TABLE IF NOT EXISTS chores (
+    //     id INT NOT NULL,
+    //     text TEXT NOT NULL,
+    //     rarity ENUM('c','u','r','s'),
+    //     PRIMARY KEY id
+    // )",
+    // )
+    // .unwrap();
+
     assert_eq!(2 + 2, 4);
 }
